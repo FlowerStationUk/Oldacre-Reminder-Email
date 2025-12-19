4bdc0a3eae653192088a9404cb177af2f848c30a
KhawajaTanzeelUrRehman <77752933+KhawajaTanzeelUrRehman@users.noreply.github.com>
2025-12-19 11:01:01 +0100

    my test email

--- src/services/email.js
+++ src/services/email.js
@@ -11,7 +11,7 @@ export const sendReminderEmail = async (toEmail, orderData) => {
         const htmlContent = getReminderHtml(orderData);
         const msg = {
             to: toEmail,
-            cc: 'accounts@oldacre.co.uk',
+            cc: 'accounts@oldacre.co.uk, khawajatn1@gmail.com',
             from: 'info@oldacre.co.uk',
             subject: `Payment Reminder: Order ${orderData.name}`,
             html: htmlContent,
